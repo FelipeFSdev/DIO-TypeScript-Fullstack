254239322af41845a7e1c21575a263380eb7782a
Felipe <felipe.a.melo@hotmail.com>
2024-01-11 16:13:02 -0300

    Introdução ao react

---
+++ Material de estudo/Aula 03 DIO - Introducao ao React/Aula 03 DIO - Introducao ao React/my-app-ts/src/components/Card.tsx
@@ -0,0 +1,10 @@
+export const Card = () => {
+    return (
+        <div>
+            <h1>Component Card Title</h1>
+            <h2> Component Card Subtitle</h2>
+            <p>Component Card Paragraph</p>
+            <p> It can be reused how many times i want</p>
+        </div>
+    )
+}
