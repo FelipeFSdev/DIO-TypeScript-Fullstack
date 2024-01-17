29befb1593ed2430b8686f04c3d5398bd608ccfc
Felipe <felipe.a.melo@hotmail.com>
2024-01-17 14:40:12 -0300

    Image, color, button and email/senha improvement

--- Material de estudo/Aula 05 DIO - Criando uma pagina web/Projeto basico 02 - DiO bank/pagina-web-ts/src/App.tsx
+++ Material de estudo/Aula 05 DIO - Criando uma pagina web/Projeto basico 02 - DiO bank/pagina-web-ts/src/App.tsx
@@ -1,13 +1,27 @@
-import { Layout } from './Components/Layout';
-
-import { ChakraProvider, Input } from '@chakra-ui/react';
+import { Box, Button, Center, ChakraProvider, Image, Input } from '@chakra-ui/react';
 
 function App() {
   return (
     <ChakraProvider>
-      <h1> Realize login antes de continuar </h1>
-      <Input placeholder='email' />
-      <Input placeholder='senha' />
+      <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+        <Box backgroundColor='white' borderRadius='10px' padding='15px'>
+          <Center>
+            <Image
+              src='https://lp.dio.me/wp-content/uploads/2023/03/LOGO-DIO-COLOR.png' alt='DiO logo'
+              boxSize='5%'
+              marginBottom='5px'
+            />
+            <h1> Realize login antes de continuar </h1>
+          </Center>
+          <form>
+            <Input type='email' placeholder='email' />
+            <Input type='password' placeholder='senha' />
+            <Button colorScheme='blue' size='sm' marginTop='5px' type='submit'>
+              Login
+            </Button>
+          </form>
+        </Box>
+      </Box>
     </ChakraProvider>
   );
 }
